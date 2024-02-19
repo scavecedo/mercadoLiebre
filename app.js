ba7f35a469acf693d6b93b4da09aac6865b4e32f
--global <--global>
2024-02-19 17:21:52 -0300

    feat: creado el servidor de Express y estructura de carpetas

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.resolve("./public")));
+app.listen(3000,() => console.log("Server corriendo en port 3000"));
+app.get ("/", (req,res) => {
+    res.sendFile(path.resolve("./views/home.html"));
+})
