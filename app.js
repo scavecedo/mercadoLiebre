3afbcda1cd9ff4510214fefca8b5a87c5f2e1c2e
--global <--global>
2024-02-22 13:54:13 -0300

    feat/parte del formulario de creacion de usuario

--- app.js
+++ app.js
@@ -7,3 +7,6 @@ app.listen(3000,() => console.log("Server corriendo en port 3000"));
 app.get ("/", (req,res) => {
     res.sendFile(path.resolve("./views/home.html"));
 })
+app.get ("/register", (req,res) => {
+    res.sendFile(path.resolve("./views/register.html"));
+})
