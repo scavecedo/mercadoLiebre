6a5fd2fd19fd0feb6bf39a75d5fd05d9a635c14b
--global <--global>
2024-02-27 21:42:50 -0300

    feat/agregando config para render

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(path.resolve("./public")));
-app.listen(3000,() => console.log("Server corriendo en port 3000"));
+
+const port = process.env.PORT || 3000
+
+app.listen(port,() => console.log(`Server corriendo en port ${port}`));
 app.get ("/", (req,res) => {
     res.sendFile(path.resolve("./views/home.html"));
 })
